95bd71cf707a02ec60988ab8a843d59eac92378e
Kenton Carrier <kncarrier28@gmail.com>
2020-07-15 13:43:37 -0400

    feat: Abstract output into a function

--- timeforacceleration.py
+++ timeforacceleration.py
@@ -1,6 +1,7 @@
 # sys for argument acceptance, math for operations
 import sys
 import math
+from decimal import Decimal
 
 # Globals
 # earth radius in meters (6.37 million)
@@ -9,6 +10,14 @@ r_earth = 6370000
 c_speed = 299792458
 # constant in meters per second, squared
 grav_const = 9.80665
+
+
+def printOutput(alt, iter, time_total, vel_total):
+    print("Altitude: ", alt, "m | ", "Iterator: ", iter, " m")
+    print("Time (hrs): ", round((time_total / 3600), 2), " hrs")
+    print("Time total: ", time_total, " s")
+    print("Final velocity: ", round(vel_total, 3), " m/s")
+    print("Speed of light: ", c_speed, " m/s | ", round((vel_total / c_speed), 3), "c")
 
 
 def calcGravity(alt_inc):
@@ -20,8 +29,10 @@ def calcGravity(alt_inc):
 def main(iter, alt):
 
     # cast arguments to floats to avoid bugs
-    alt = float(alt) * 1000 # convert kilometers to meters
-    iter = float(iter) # already in meters or fractions of meters
+    # convert kilometers to meters
+    alt = Decimal(alt) * 1000
+    # already in meters or fractions of meters
+    iter = Decimal(iter)
 
     inc_count = 0 # iteration counter for debugging
 
@@ -42,14 +53,6 @@ def main(iter, alt):
         time_inc = math.sqrt((2 * iter) / grav_new)
         time_inc = round(time_inc, 4)
 
-        # print for debug
-        # if (inc_count < 10): # print first 100 iterations
-        #     print("Timestep: ", time_inc)
-        #     print("Elapsed time: ", time_total)
-        #     print("Distance: ", iter)
-        #     print("Elapsed distance: ", (alt - alt_inc))
-        #     print("Earth gravity: ", grav_new, "\n")
-
         # add time increment to total
         time_total = time_total + time_inc
 
@@ -62,15 +65,7 @@ def main(iter, alt):
         vel_total += (grav_new * time_inc)
         inc_count += 1
 
-    print("Altitude: ", alt, "m | ", "Iterator: ", iter, " m")
-    # divide by 60s and 60m to get hours
-    print("Time (hrs): ", round((time_total / 3600), 2), " hrs")
-    print("Time total: ", time_total, " s")
-    print("Iterations: ", inc_count)
-    print("Final velocity: ", round(vel_total, 3), " m/s")
-    print("Speed of light: ", c_speed, " m/s | ", round((vel_total / c_speed), 3), "c")
-
-# end main() -------------------------------------------------------------------
+    printOutput(alt, iter, time_total, vel_total)
 
 main(sys.argv[1], sys.argv[2])
 
