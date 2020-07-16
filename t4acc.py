99afb4b2d48112f226b37879046475aba68bcc04
Cassini Griffin <endallmemes@gmail.com>
2020-07-15 22:41:26 -0400

    Fix: fixed not accounting for accumulated velocity attempted refactor in separate file, encountered TypeError: "can't multiply sequence by non-int of type 'float'". full overhaul attempted (t4acc.py), encountered same error.

--- t4acc.py
+++ t4acc.py
@@ -1,15 +1,84 @@
 import sys
 import math
+from decimal import Decimal
 
-# need to separate and blackbox
-# what functions do we need?
-# one to initialize variables/accept inputs
+# global variables
+# earth radius in meters (6.37 million)
+r_earth = Decimal(6370000)
+# speed of light in m/s
+c_speed = Decimal(299792458)
+# constant in meters per second, squared
+grav_const = Decimal(9.80665)
+# iteration count
+iter_count = int(0)
 
-def data_init(iter, alt):
-    alt = float(alt) * 1000
-    iter = float(iter)
+def printOutputs(oStore):
+    for i in oStore:
+        print(i, "\n")
 
-    incr_count = 0
+def dataInit(alt, iter):
+    alt = Decimal(alt) * 1000
+    alt_incr = alt
+    grav_constant = Decimal(9.80665)
+    grav_new = grav_constant
+    time_total = Decimal(0)
+    time_incr = time_total
+    vel_total = Decimal(0)
+    vel_incr = vel_total
+    data = [alt, alt_incr, grav_constant, grav_new, time_total, time_incr, vel_total, vel_incr, iter]
+        # 0 alt
+        # 1 alt_incr
+        # 2 grav_constant
+        # 3 grav_new
+        # 4 time_total
+        # 5 time_incr
+        # 6 vel_total
+        # 7 vel_incr
+        # 8 iterator
+    return data
 
-    alt_inc = alt
-    r_earth = 6370000
+def calcGravity(gStore):
+    gStore[3] = ((r_earth / (r_earth + dataStore[1])) ** 2)
+    gStore[3] *= gStore[2]
+    return gStore
+
+def calcTime(tStore):
+    # tStore[5] = -1 * (math.sqrt((2 * tStore[8] * tStore[3]) + (tStore[7] ** 2) + tStore[7]) / tStore[3])
+
+    tStore[5] = tStore[8] * tStore[3]
+    tStore[5] *= 2
+    tStore[5] += (tStore[7] ** 2)
+    tStore[5] = math.sqrt(tStore[5])
+    tStore[5] += tStore[7]
+    tStore[5] /= tStore[3]
+    tStore[5] *= (-1)
+
+    return tStore
+
+def calcVelocity(vStore):
+    vStore[7] = vStore[3] * vStore[5]
+    return vStore
+
+def main(altitude, iterator):
+
+    dataStore = dataInit(altitude, iterator)
+
+    while (dataStore[1] > 0):
+        dataStore = calcTime(dataStore)
+        dataStore = calcGravity(dataStore)
+        dataStore = calcVelocity(dataStore)
+
+        # decrement altitude
+        dataStore[1] -= dataStore[8]
+        # increment time
+        dataStore[4] += dataStore[5]
+        # increment velocity
+        dataStore[6] += dataStore[7]
+        # increment counter
+        iter_count += 1
+
+    printOutputs(dataStore)
+    print(iter_count)
+# /main
+
+main(sys.argv[1], sys.argv[2])
---
+++ timeforaccelerationv2.py
@@ -0,0 +1,86 @@
+# sys for argument acceptance, math for operations
+import sys
+import math
+
+# Globals
+# earth radius in meters (6.37 million)
+r_earth = 6370000
+# speed of light in m/s
+c_speed = 299792458
+# constant in meters per second, squared
+grav_const = 9.80665
+
+
+def printOutput(alt, iter, time_total, vel_total, inc_count):
+    print("Altitude: ", alt, "m | ", "Iterator: ", iter, " m")
+    print("Time (hrs): ", round((time_total / 3600), 2), " hrs")
+    print("Time total: ", round(time_total, 2), " s")
+    print("Final velocity: ", round(vel_total, 3), " m/s | ", round((vel_total / c_speed), 3), "c")
+    print("Speed of light: ", c_speed, " m/s")
+    print("Iterations: ", inc_count)
+
+def calcGravity(alt_inc):
+    grav_new = ((r_earth / (r_earth + alt_inc)) ** 2)
+    grav_new *= grav_const
+    return grav_new
+
+def calcTime(time_inc, iter, grav_new, vel_inc):
+    time_inc = -((math.sqrt((2 * iter * grav_new) + math.pow(vel_inc, 2)) + vel_inc) / grav_new)
+    # time_inc = math.sqrt((2 * iter) / grav_new)
+    return time_inc
+
+def calcVelocity(grav_new, time_inc):
+    vel_inc = (grav_new * time_inc)
+    return vel_inc
+
+
+def main(iter, alt):
+
+    # cast arguments to floats to avoid bugs
+    # convert kilometers to meters
+    alt = alt * 1000
+    # already in meters or fractions of meters
+
+    inc_count = 0 # iteration counter for debugging
+
+    alt_inc = alt # assign total altitude to counter variable
+
+    grav_new = calcGravity(alt_inc) # initial gravity at 0 velocity
+
+    time_inc = 0 # time increment in fractions of seconds
+    time_total = 0 # time totaled from increments
+
+    vel_inc = calcVelocity(grav_new, time_inc) # velocity at each step
+    vel_total = 0 # total velocity summed from acceleration operations
+
+    while (alt_inc > 0):
+        # import pdb; pdb.set_trace() # start debug
+
+        # find time taken to travel iter given grav_new
+        time_inc = calcTime(time_inc, iter, grav_new, vel_inc)
+
+        # add time increment to total
+        time_total += time_inc
+
+        # find new altitude
+        alt_inc -= iter
+
+        # re-evaluate gravity at new altitude
+        grav_new = calcGravity(alt_inc)
+
+        vel_inc = calcVelocity(grav_new, time_inc)
+
+        vel_total += vel_inc
+
+        inc_count += 1
+
+        # if (inc_count < 10):
+        #     print(time_inc, "\n", time_total)
+    # /while
+
+    printOutput(alt, iter, time_total, vel_total, inc_count)
+# /main
+
+main(sys.argv[1], sys.argv[2])
+
+# py timeforacceleration.py .1 400
