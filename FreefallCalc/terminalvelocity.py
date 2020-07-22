edeab6dc7a9367999c4e76cbb91cfde010e5b853
Cassini Griffin <endallmemes@gmail.com>
2020-07-22 15:44:10 -0400

    Fixed: problems with the array comparator little bit more work and the terminal velocity will be done

--- FreefallCalc/terminalvelocity.py
+++ FreefallCalc/terminalvelocity.py
@@ -5,13 +5,13 @@ import math
 mass = 361779338 # in kilograms
 cross_area = 86987.4 # in meters^2
 drag_coeff = 1.42 # of hemisphere opposite flow
+radius_earth = float(6370000) # earth radius in meters (6.37 million)
+gravity_const = float(9.80665) # constant in meters per second, squared
 
-# earth radius in meters (6.37 million)
-radius_earth = float(6370000)
-# constant in meters per second, squared
-gravity_const = float(9.80665)
+term_vel = 0
+density_value = 0
 
-altitude_density = [[0, 1.225], [1000, 1.112], [2000, 1.007], [3000, 0.9093], [4000, 0.8194], [5000, 0.7364], [6000, 0.6601], [7000, 0.59], [8000, 0.5258], [9000, 0.4671], [10000, 0.4135], [15000, 0.1948], [20000, 0.08891], [25000, 0.04008], [30000, 0.01841], [40000, 0.003996], [50000, 0.001027], [60000, 0.0003097], [70000, .00008283], [80000, .00001846]]
+altitude_density = [[80000, 1.846e-05], [70000, 8.283e-05], [60000, 0.0003097], [50000, 0.001027], [40000, 0.003996], [30000, 0.01841], [25000, 0.04008], [20000, 0.08891], [15000, 0.1948], [10000, 0.4135], [9000, 0.4671], [8000, 0.5258], [7000, 0.59], [6000, 0.6601], [5000, 0.7364], [4000, 0.8194], [3000, 0.9093], [2000, 1.007], [1000, 1.112], [0, 1.225]]
 # in meters and kg/m^3
 
 def FindTerminal(gravity, density):
@@ -22,19 +22,24 @@ def CalcGravityStuff(altitude):
     gravity = gravity_const * math.pow((radius_earth / (radius_earth + altitude)), 2)
     return gravity
 
-def main():
-    term_vel = 0
-    # import pdb; pdb.set_trace()
-    altitude_density.reverse()
+def main(alt_input):
+    gravity_new = CalcGravityStuff(alt_input)
+
     for r in range(0, len(altitude_density) - 1):
-        print(term_vel)
-        # print(altitude_density[r][0])
-        # print(altitude_density[r][1], "\n")
-        gravity_new = CalcGravityStuff(altitude_density[r][0])
-        term_vel += FindTerminal(gravity_new, altitude_density[r][1])
+        if alt_input < altitude_density[r][0] and alt_input >= altitude_density[r+1][0]:
+            density_value = altitude_density[r+1][1]
+            break
+        elif alt_input > altitude_density[0][0]: # aka if altitude is too damn high
+            density_value = altitude_density[0][1] # set it to lowest possible value
+            break
 
-    print(round(term_vel / 20, 2), "m/s average")
-    print("Don't do an average, instead output a different speed for each altitude range!")
+    term_vel = FindTerminal(gravity_new, density_value)
+    print(term_vel)
+    return term_vel
 
-main()
-# use the array, use a for loop to iterate on each altitude
+# main(500)
+# main(1000)
+# main(1500)
+# main(40000)
+# main(81000)
+# test cases
