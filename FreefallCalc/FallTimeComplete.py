61d7cbbe91a5ea730e1d2b2b53981e84b2b71c79
Cassini Griffin <endallmemes@gmail.com>
2020-07-23 13:33:20 -0400

    Fix: added ability to limit velocity since this is for a space elevator, it wouldnt experience constant freefall -- this update allows me to account for that

--- FreefallCalc/FallTimeComplete.py
+++ FreefallCalc/FallTimeComplete.py
@@ -13,8 +13,10 @@ gravity_const = float(9.80665)
 # terminal velocity globals
 mass = 361779338 # in kilograms
 cross_area = 86987.4 # in meters^2
-# drag_coeff = 1.42 # semispehere opposite flow
-drag_coeff = 2.1 # rectangular box
+drag_coeff = 1.42 # semisphere opposite flow -- 2097s for 4000m at .5g
+# drag_coeff = 2.1 # rectangular box -- this gives me 2117s for 4000km at .5g
+# drag_coeff = 10 # what the fuck -- this gives me a time of 2200s for 4000km at .5g
+# oddly, this isn't a huge factor
 
 altitude_density = [[80000, 1.846e-05], [70000, 8.283e-05], [60000, 0.0003097], [50000, 0.001027], [40000, 0.003996], [30000, 0.01841], [25000, 0.04008], [20000, 0.08891], [15000, 0.1948], [10000, 0.4135], [9000, 0.4671], [8000, 0.5258], [7000, 0.59], [6000, 0.6601], [5000, 0.7364], [4000, 0.8194], [3000, 0.9093], [2000, 1.007], [1000, 1.112], [0, 1.225]]
 
@@ -103,9 +105,18 @@ def main(altitude, iterator):
         # define terminal velocity here
         if velocity_counter > terminal_velocity: # if velocity would exceed terminal
             velocity_counter = terminal_velocity
+            # who said they have to accelerate at all?
+            # why not just Not Accelerate? Cap the velocity?
+            # or accelerate on an extremely small scale
+            # they might end up going slow but who cares
+            # to fall 500km in 12 hours, you would need to travel
+            # 500,000m in 43,200s or about 11.5 m/s
+        velocity_counter = 11 # m/s, or 24.6 mph
+        # this should allow for a ~12hr fall from 500km
 
         gravity_new = CalculateGravity(alt_counter)
-        # gravity_new *= 0.5 # fraction of total
+
+
 
         loop_count += 1
 
