e3cdaa0946471eca8e6775e6e1d7bb4ed7752704
Cassini Griffin <endallmemes@gmail.com>
2020-07-28 11:25:50 -0400

    Added: ability to calculate over distance range & for different bodies/objects given their mass, drag coefficient, and frontal area

--- FreefallCalc/FallTimeComplete.py
+++ FreefallCalc/FallTimeComplete.py
@@ -11,12 +11,15 @@ lunar_dist = float(384402000) # average distance to moon
 gravity_const = float(9.80665)
 
 # terminal velocity globals
+# climber attributes
 mass = 361779338 # in kilograms
 cross_area = 86987.4 # in meters^2
 drag_coeff = 1.42 # semisphere opposite flow -- 2097s for 4000m at .5g
-# drag_coeff = 2.1 # rectangular box -- this gives me 2117s for 4000km at .5g
-# drag_coeff = 10 # what the fuck -- this gives me a time of 2200s for 4000km at .5g
-# oddly, this isn't a huge factor
+
+# glider attributes
+# mass = 1000 # in kilograms
+# cross_area = 3.14159 # in meters^2
+# drag_coeff = 0.04 # streamlined body
 
 altitude_density = [[80000, 1.846e-05], [70000, 8.283e-05], [60000, 0.0003097], [50000, 0.001027], [40000, 0.003996], [30000, 0.01841], [25000, 0.04008], [20000, 0.08891], [15000, 0.1948], [10000, 0.4135], [9000, 0.4671], [8000, 0.5258], [7000, 0.59], [6000, 0.6601], [5000, 0.7364], [4000, 0.8194], [3000, 0.9093], [2000, 1.007], [1000, 1.112], [0, 1.225]]
 
@@ -33,7 +36,7 @@ def clear():
 
 # print data in nice formatting
 def PrintOut(altitude, iterator, time, velocity, loops):
-    print("\n", "      Altitude: ", altitude, " m", "\n", "Lunar Distance: ", lunar_dist, " m", "\n", "--------------| ", round((altitude/lunar_dist) * 100, 2), "% of lunar distance", "\n", "      Iterator: ", iterator, " m", "\n", "  Time elapsed: ", round(time, 8), " seconds", "\n", "--------------| ", round((time / 3600.0), 3), " hours", "\n", "Final velocity: ", round(velocity, 8), " m/s", "\n", "    Loop count: ", loops)
+    print("\n", "      Altitude: ", altitude, " m", "\n", "Lunar Distance: ", lunar_dist, " m", "\n", "--------------| ", round((altitude/lunar_dist) * 100, 2), "% of lunar distance", "\n", "      Iterator: ", iterator, " m", "\n", "  Time elapsed: ", round(time, 8), " seconds", "\n", "--------------| ", round(time/60.0, 3), " minutes", "\n", "--------------| ", round((time / 3600.0), 3), " hours", "\n", "Final velocity: ", round(velocity, 8), " m/s", "\n", "    Loop count: ", loops)
 
 # calculate gravity at new altitude
 def CalculateGravity(altitude):
@@ -91,6 +94,9 @@ def main(altitude, iterator):
     loop_count = int(0)
 
     while (alt_counter > 0):
+    # while (alt_counter > 250000):
+        # change 0 to whatever altitude you want to end at
+        # e.g. 500 -> 250km, stop altitude at 250,000m
 
         time_counter = CalculateTime(iterator, gravity_new, velocity_counter)
         time_total += time_counter
@@ -111,15 +117,13 @@ def main(altitude, iterator):
             # they might end up going slow but who cares
             # to fall 500km in 12 hours, you would need to travel
             # 500,000m in 43,200s or about 11.5 m/s
+
         velocity_counter = 11 # m/s, or 24.6 mph
         # this should allow for a ~12hr fall from 500km
 
         gravity_new = CalculateGravity(alt_counter)
 
-
-
         loop_count += 1
-
 
         if iterator == 0.1:
             modulo = 10000
