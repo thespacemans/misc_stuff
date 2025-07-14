33ef83ec4451a8a5325bf4f56655c0d59e4b1d96
Cassini Griffin <endallmemes@gmail.com>
2025-07-13 23:54:59 -0400

    Fixed some conflicts between online and local repo

---
+++ create_fart.bat
@@ -0,0 +1,8 @@
+echo shittington fart > Fortbuscus.txt
+@echo OFF
+echo: & @echo: & @echo: & @echo: 
+echo FART BUSCUS CREATED. & @echo: 
+echo: & @echo: & @echo: & @echo: 
+echo Press any key to kill Fortbuscus.
+pause >nul
+del /q Fortbuscus.txt
